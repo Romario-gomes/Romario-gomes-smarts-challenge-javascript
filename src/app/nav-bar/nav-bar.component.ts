979846c2b0b567c002777dd17786dfd97df5e2fb
Romario Alves <Romariogn10@gmail.com>
2021-01-06 18:20:01 -0300

    header dates front concluidos

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { templateJitUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-nav-bar',
+    templateUrl: './nav-bar.component.html',
+    styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent{
+
+}
